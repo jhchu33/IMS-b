6931fc1f66e64faa8e95a1ebf8e26aad4cc5fef9
jhchu <674739233@qq.com>
2019-01-09 10:10:23 +0800

    Change UserController

---
+++ src/main/java/controller/UserController.java
@@ -0,0 +1,28 @@
+package controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+    @PostMapping("/signin")
+    publc int signin(){
+
+    }
+
+    @PostMapping("/signup")
+    public int signup(){
+
+    }
+
+    @GetMapping("/profile")
+    public int getUser(){
+
+        return 2;
+    }
+
+}
